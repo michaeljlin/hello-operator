02f60b10561578249bc7576452d7865f88e4102c
R-Brewster <rbrewster@ucmerced.edu>
2017-11-03 11:22:04 -0700

    Completed addition of comm panels

--- src/components/spymaster_ui.js
+++ src/components/spymaster_ui.js
@@ -1,37 +1,35 @@
 import React, {Component} from 'react';
-import './ui.css'
+// import './ui.css';
 import monitor from '../assets/images/monitor.png';
+import ComPanel from './com_panel';
 
 class spymasterUI extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            leftComPanel: new ComPanel().state,
+            rightComPanel: new ComPanel().state,
+        };
+    }
+
+    componentDidMount(){
+        this.setState ({
+            leftComPanel: {
+                displayText: 267884005
+            },
+            rightComPanel: {
+                displayText: 748716483
+            }
+        })
+    }
+
     render(){
+
         return (
             <div id="spymasterUiContainer">
                 <img id="monitor" src={monitor}/>
-                {/*<img id="metalBackground" src={metalBackground}/>*/}
-                <div id="leftPanel">
-                    <div className="display"> 6482046</div>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                </div>
-                <div id="rightPanel">
-                    <div className="display">26072548 </div>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn primary"> </button>
-                    <button className="btn danger"> </button>
-                </div>
+                <ComPanel id="leftPanel"  displayText={this.state.leftComPanel.displayText} />
+                <ComPanel id="rightPanel" displayText={this.state.rightComPanel.displayText} />
             </div>
             )
     }
