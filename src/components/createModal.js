1619e3f5ec4af812c8fcba37099bf8c54b1707d0
R-Brewster <rbrewster@ucmerced.edu>
2017-11-04 14:35:07 -0700

    Made the spy modal collapsible, and altered its position

--- src/components/createModal.js
+++ src/components/createModal.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import ComPanel from './com_panel';
-// import Modal from './createModal';
+import cogGlyphicon from '../assets/images/cog_glyphicon.png';
+import closeGlyphicon from '../assets/images/close_glyphicon.png';
 
 
 class CreateModal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            visibility: 'none',
+            modalVisibility: 'none',
+            glyphiconVisibility: 'inline-block'
         };
 
         this.openModal = this.openModal.bind(this);
@@ -16,36 +18,31 @@ class CreateModal extends Component {
 
     openModal() {
         this.setState({
-            visibility: 'block',
+            modalVisibility: 'block',
+            glyphiconVisibility: 'none',
         });
-
-        const modalStyle = {
-            display: 'block'
-        }
-
     }
 
     closeModal() {
         this.setState({
-            visibility: 'none',
+            modalVisibility: 'none',
+            glyphiconVisibility: 'inline-block'
         });
-
-        const modalStyle = {
-            display: 'none',
-        }
     }
 
     render() {
-        const modalStyle = this.state.visibility;
+        const modalStyle = this.state.modalVisibility;
+        const modalButtonStyle = this.state.glyphiconVisibility;
 
         return (
             <div>
                 <button id="openModal" onClick={this.openModal}>
-                    <img />
+                    <img id="glyphicon" src={cogGlyphicon} style={{display: modalButtonStyle}}/>
                 </button>
-                <div style={{display: modalStyle}}>
-                    <h1>Modal content</h1>
-                    <button onClick={this.closeModal}>Close</button>
+                <div id="spyModal" style={{display: modalStyle}}>
+                    {/*<h1>Modal content</h1>*/}
+                    <ComPanel id="spyModalComPanel"/>
+                    <img id="spyModalClose" src={closeGlyphicon} onClick={this.closeModal} style={{display: modalStyle}}/>
                 </div>
             </div>
         )
