bafc717342df0743a07003fb266e6a1a9da21bff
R-Brewster <rbrewster@ucmerced.edu>
2018-02-17 17:03:55 -0800

    About page setup

---
+++ client/src/components/about.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link, Route, Redirect } from 'react-router-dom';
+
+
+class About extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        };
+
+
+    }
+
+
+
+    render(){
+
+            return (
+                <div className="landing">
+
+                </div>
+            );
+        }
+
+
+}
+
+
+
+export default About;
