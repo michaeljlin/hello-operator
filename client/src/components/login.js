6903ff10ecc535d2cc54517849e6c996955a5869
michaeljlin <mjameslin@gmail.com>
2018-03-12 15:37:24 -0700

    Finished styling new signup form

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -21,10 +21,14 @@ class SignUp extends Component {
 
     checkInput({input, type, meta:{touched, error}}){
         let inputHolder = "Null";
-        
+        let inputStyle = {
+            width: '100%'
+        };
+
         switch(input.name){
             case 'first_name':
                 inputHolder = "First Name";
+                inputStyle.width = '95%';
                 break;
             case 'last_name':
                 inputHolder = "Last Name";
@@ -46,7 +50,7 @@ class SignUp extends Component {
 
         return (
             <div>
-                <input placeholder={inputHolder} {...input} type={type} />
+                <input style={inputStyle} placeholder={inputHolder} {...input} type={type} />
                 {touched && (error && <p>{error}</p>)}
             </div>
         )
@@ -97,10 +101,10 @@ class SignUp extends Component {
                 <div id="login_signup_container">
                 <h1>Sign Up:</h1>
                     <form id="signUpForm" onSubmit={handleSubmit((vals) => this.submitButtonClicked(vals))}>
-                        <div className="inputContainer">
+                        <div className="nameInputContainer">
                             <Field id="input_first_name" component={this.checkInput} type="text" name="first_name"/>
                         </div>
-                        <div className="inputContainer">
+                        <div className="nameInputContainer">
                             <Field id="input_last_name" component={this.checkInput}  type="text" name="last_name"/>
                         </div>
                         <div className="inputContainer">
@@ -134,21 +138,26 @@ function validate(values) {
         error.first_name = 'Please enter your first name';
     }
     if(!values.last_name){
-        error.last_name = 'Please enter your last name'
+        error.last_name = 'Please enter your last name';
     }
     if(!values.username ){
-        error.username = 'Please enter your username'
+        error.username = 'Please enter your username';
     }
 
     if( values.username !== undefined && !(values.username).match(/^(?![_\d])[\-\w!@#$%^&*]{6,}$/)){
-        error.username = 'Your username must have at least 6 characters and cannot start with an underscore or number.'
+        error.username = 'Your username must have at least 6 characters and cannot start with an underscore or number.';
     }
 
     if(!values.email){
-        error.email = 'Please enter your email'
+        error.email = 'Please enter your email';
     }
+
+    if(values.email !== undefined && !(values.email).match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        error.email = 'Please enter a valid email address';
+    }
+
     if(!values.password){
-        error.password = 'Please enter your password'
+        error.password = 'Please enter your password';
     }
 
     if( values.password !== undefined && !(values.password).match(/^(?![_\d])[\-\w~!@#$%^&*]{8,}$/)){
@@ -156,14 +165,14 @@ function validate(values) {
         // if((values.password).match(/^[\-\w]{0,7}$/)){
         //     error.password = 'Your password does not have at least 8 characters.'
         // }
-        error.password = 'Your password does not have at least 8 characters.'
+        error.password = 'Your password does not have at least 8 characters.';
     }
 
     if(!values.confirm_password){
-        error.confirm_password = 'Please confirm your password'
+        error.confirm_password = 'Please confirm your password';
     }
     if(values.password !== values.confirm_password){
-        error.confirm_password = 'Passwords do not match'
+        error.confirm_password = 'Passwords do not match';
     }
 
     return error;
